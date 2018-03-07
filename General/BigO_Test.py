bdb2b2cd4ff4156a408b0cfb613a549ca3c2389d
srikloud <34868217+srikloud@users.noreply.github.com>
2018-03-07 05:28:30 -0800

    Update BigO_Test.py

--- General/BigO_Test.py
+++ General/BigO_Test.py
@@ -1,6 +1,18 @@
 
 # Test Big O(N)
 
+O(1) (worst case): Given the page that a business's name is on and the business name, find the phone number.
+
+O(1) (average case): Given the page that a person's name is on and their name, find the phone number.
+
+O(log n): Given a person's name, find the phone number by picking a random point about halfway through the part of the book you haven't searched yet, then checking to see whether the person's name is at that point. Then repeat the process about halfway through the part of the book where the person's name lies. (This is a binary search for a person's name.)
+
+O(n): Find all people whose phone numbers contain the digit "5".
+
+O(n): Given a phone number, find the person or business with that number.
+
+O(n log n): There was a mix-up at the printer's office, and our phone book had all its pages inserted in a random order. Fix the ordering so that it's correct by looking at the first name on each page and then putting that page in the appropriate spot in a new, empty phone book.
+
 import time
 l = list()
 for i in range(0,100000):
